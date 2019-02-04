412e9ca33d54e8e834afd8e3425cb7090dd10612
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2019-02-04 12:58:45 +0600

    Add files via upload

--- Decision Tree/DecisionTree.py
+++ Decision Tree/DecisionTree.py
@@ -1,1 +1,69 @@
+import math
 
+
+def main():
+    classes, dataset = extractClass()
+    print(classes)
+    print()
+    initialEntropy = entropyInDataset(dataset, classes)
+    root = splitData(dataset, initialEntropy)
+    #splitData(dataset, initialEntropy)
+
+
+def extractClass():
+    file = open("iris.txt", "r+")
+    fullData = file.readlines()
+    dataset = []
+    classSet = set()
+    for line in fullData:
+        line = line.split(',')
+        dataset.append(line)
+        classSet.add(int(line[-1]))
+
+    return classSet, dataset
+
+
+def entropyInDataset(dataset, classes):
+    totalData = len(dataset)
+    print(totalData)
+
+    entropy = 0.0
+    for value in classes:
+        occurences = [int(row[-1]) for row in dataset].count(value)
+        print(occurences)
+        probability = occurences / totalData
+        print(probability)
+        print(math.log2(probability))
+        entropy += (-probability * math.log2(probability))
+        print(entropy)
+    return entropy
+
+
+def splitData(dataset, initialEntropy):
+    indices = len(dataset[0])
+    print(indices)
+    leftSet, rightSet = list(), list()
+
+    for row in dataset:
+        for index in range(indices - 1):
+            leftSet, rightSet = getTwoSet(dataset, index, float(row[index]))
+
+    print(leftSet)
+    print()
+    print(rightSet)
+    return root = None
+    
+def getTwoSet(dataset, index, indexValue):
+    leftSet, rightSet = list(), list()
+
+    for row in dataset:
+        if float(row[index]) < indexValue:
+            leftSet.append(row)
+        else:
+            rightSet.append(row)
+
+    return leftSet, rightSet
+
+
+if __name__ == "__main__":
+    main()
