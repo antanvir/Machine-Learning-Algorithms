1e4515c1b6a7ca6d841562c9748328fea4ca214b
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2019-02-05 15:39:33 +0600

    Add files via upload

--- Apriori Algorithm/Apriori.py
+++ Apriori Algorithm/Apriori.py
@@ -2,31 +2,10 @@ def main():
 	dataset = readFile()
 	#print(dataset)
 	L1 = find_frequent_1_itemsets(dataset)
-	L1Counter = SupportCounter(dataset, L1, 2)
-	Itemsets = apriori(dataset, L1)
+	#L1Counter = SupportCounter(dataset, L1, 2)
+	Itemsets = apriori(dataset)
 
 
-def SupportCounter(dataset, L, k):
-	counter = list()
-	for a in L:
-		count = 0
-		items = a.split(",")
-		for bought in dataset:
-			flag = True
-			for i in range(len(items)):
-				print(items[i], bought)
-				if items[i] not in bought:
-					print("not matched")
-					flag = False
-					break
-			'''if [items[i] in bought for i in range(len(items))]:
-				count += 1'''
-			if flag == True:
-				count += 1
-
-		counter.append(count)
-	print(counter)
-	return counter
 
 
 
@@ -46,47 +25,72 @@ def readFile():
 	return dataset
 	
 
-def apriori(dataset, L1):
+
+
+
+def apriori(dataset):
+	threshold = 2
 	resultLevel = list()
 	resultCounter = list()
-	#L = find_frequent_1_itemsets(dataset)
-	#print(L)
 
-	L = L1
-	print("L: "+L)
-	#while L:
-	candidate = candidate_gen(L)
+	candidate = find_frequent_1_itemsets(dataset)
+	Level, L_count = SupportCounter(dataset, candidate, threshold)
+	print(L_count)
+	print(Level)
+
+	#while Level:
+	candidate = candidate_gen(Level, 2)
 	print(candidate)
+	Level, L_count = SupportCounter(dataset, candidate, threshold)
+	print(L_count)
+	print(Level)
 
 
-	return result
+
+	return resultLevel, resultCounter
 
 
-def candidate_gen(L):
+def SupportCounter(dataset, L, threshold):
+	counter = list()
+	Level = list()
+	for a in L:
+		count = 0
+		items = a.split(",")
+		for bought in dataset:
+			flag = True
+			for i in range(len(items)):
+				#print(items[i], bought)
+				if items[i] not in bought:
+					#print("not matched")
+					flag = False
+					break
+
+			if flag == True:
+				count += 1
+
+		if count >= threshold:
+			counter.append(count)
+			Level.append(a)
+	print(counter)
+	return Level, counter
+
+
+
+
+
+def candidate_gen(L, k):
 	candidate = set()
-	#print(L)
 
-	'''length = len(L)
-	print(length)
-	for i in range(length):
-		for j in range(i+1, length):
-			itemsI = L[i].split(",")
-			itemsJ = L[j].split(",")
-			#print(itemsI[-1], itemsJ[-1])
-
-			if itemsI[-1] < itemsJ[-1] and [itemsI[x] == itemsJ[x] for x in range(len(itemsI)-1)]:
-				new = L[i] + "," + itemsJ[-1]
-				print(new)
-				candidate.add(new)
-				#if has_infrequent_subset(new, L):
-					#del candidate[-1]'''
 
 	length = len(L)
 	for i in range(length):
 		for j in range(i+1, length):
 			#new = L[i]|L[j]
-			new = L[i] + "," + L[j]
-			candidate.add(new)
+			itemsI = L[i].split(",")
+			itemsJ = L[j].split(",")
+			if [itemsI[x] == itemsJ[x] for x in range(len(itemsI))]:
+				new = L[i] + "," + L[j]
+				candidate.add(new)
 
 	return list(sorted(candidate))
 
@@ -109,7 +113,7 @@ def find_frequent_1_itemsets(dataset):
 
 	for elements in dataset:
 		items = elements.split(",")
-		print(len(items))
+		#print(len(items))
 		for i in range(len(items)):
 			level1.add( items[i] )
 
