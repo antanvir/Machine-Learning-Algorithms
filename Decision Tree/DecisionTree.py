7691dadfe870872d7bbc913a1f0267a2d80aa71a
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2019-02-18 13:46:49 +0600

    Add files via upload

--- Decision Tree/DecisionTree.py
+++ Decision Tree/DecisionTree.py
@@ -3,15 +3,16 @@ import math
 
 def main():
     classes, dataset = extractClass()
-    print(classes)
-    print()
+    # print(dataset)
+    # print()
     initialEntropy = entropyInDataset(dataset, classes)
-    root = splitData(dataset, initialEntropy)
+    root = splitData(dataset, initialEntropy, classes)
+    print(root)
     #splitData(dataset, initialEntropy)
 
 
 def extractClass():
-    file = open("iris.txt", "r+")
+    file = open("iris1.txt", "r+")
     fullData = file.readlines()
     dataset = []
     classSet = set()
@@ -25,34 +26,55 @@ def extractClass():
 
 def entropyInDataset(dataset, classes):
     totalData = len(dataset)
-    print(totalData)
+    # print(totalData)
+    if totalData == 0:
+        return 0.0
 
     entropy = 0.0
     for value in classes:
         occurences = [int(row[-1]) for row in dataset].count(value)
         print(occurences)
         probability = occurences / totalData
-        #print(probability)
-        #print(math.log2(probability))
-        entropy += (-probability * math.log2(probability))
-        print(entropy)
+        # print(probability)
+        # print(math.log2(probability))
+        if probability > 0.0:
+            entropy += (-probability * math.log2(probability))
+            print(entropy)
     return entropy
 
 
-def splitData(dataset, initialEntropy):
+def GetInformationGain(leftSet, rightSet, classes, initialEntropy):
+    infoGain = 0.0
+    entropy = 0.0
+    total = len(leftSet) + len(rightSet)
+
+    entropy += (len(leftSet) / total) * entropyInDataset(leftSet, classes)
+    entropy += (len(rightSet) / total) * entropyInDataset(rightSet, classes)
+
+    infoGain = initialEntropy - entropy
+    return infoGain
+
+
+def splitData(dataset, initialEntropy, classes):
     indices = len(dataset[0])
-    print(indices)
+    # print(indices)
     leftSet, rightSet = list(), list()
 
+    print("Splitting....")
+    gainValue = 0.0
+    position, value, left, right = -1, -1, None, None
     for row in dataset:
         for index in range(indices - 1):
             leftSet, rightSet = getTwoSet(dataset, index, float(row[index]))
+            infoGain = GetInformationGain(
+                leftSet, rightSet, classes, initialEntropy)
+            if infoGain > gainValue:
+                position, value, left, right = index, row[index], leftSet, rightSet
 
-    root = None
+    root = {"index": position, "value": value, "left": left, "right": right}
     return root
 
 
-    
 def getTwoSet(dataset, index, indexValue):
     leftSet, rightSet = list(), list()
 
