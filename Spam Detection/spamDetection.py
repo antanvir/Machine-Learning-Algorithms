7513354fe93d82f4dad3583243e8b02d7968ed5d
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2019-04-02 13:28:54 +0600

    Updated spamDetection.py

--- Spam Detection/spamDetection.py
+++ Spam Detection/spamDetection.py
@@ -1,79 +1,110 @@
 from random import randrange, seed
 from math import sqrt, ceil
 
+spamMail, nonspamMail = 0, 0
+pSpamWord = None
+pNonspamWord = None
 
 def main():
-    filename = "spambase.data"
-    dataset = readfile(filename)
-    pSpamWord, pNonspamWord = trainData(dataset)
-    # print(pSpamWord, pNonspamWord)
-    crossValidation(dataset, pSpamWord, pNonspamWord)
+	filename = "spambase.data"
+	dataset = readfile(filename)
+	pSpamWord, pNonspamWord = trainData(dataset)
+	# print(pSpamWord, pNonspamWord)
+	crossValidation(dataset, pSpamWord, pNonspamWord)
 
 
 def readfile(filename):
-    file = open(filename, "r+")
-    fullData = file.readlines()
-    dataset = []
-    for line in fullData:
-        line = line.split(',')
-        line = [float(x) for x in line]
-        dataset.append(line)
-    print(len(dataset), " ", len(dataset[1]))
-    print(dataset[1])
-    return dataset
+	file = open(filename, "r+")
+	fullData = file.readlines()
+	dataset = []
+	for line in fullData:
+		line = line.split(',')
+		line = [float(x) for x in line]
+		dataset.append(line)
+	# print(len(dataset), " ", len(dataset[1]))
+	# print(dataset[1])
+	return dataset
 
 
 def trainData(dataset):
-    spamMail, nonspamMail = 0, 0
-    for line in dataset:
-        if(line[-1]) == 1.00:
-            spamMail += 1
-        else:
-            nonspamMail += 1
+	
+	for line in dataset:
+		global spamMail, nonspamMail
+		if(line[-1]) == 1.00:
+			spamMail += 1
+		else:
+			nonspamMail += 1
 
-    print(spamMail, "\t", nonspamMail)
-    pSpamWord = [0 for x in range(len(dataset[0]) - 1)]
-    pNonspamWord = [0 for x in range(len(dataset[0]) - 1)]
-
-    # total = len(dataset)
-    for i in range(len(dataset[0]) - 1):
-        spamCount, nonspamCount = 0, 0
-        for line in dataset:
-            if(line[-1]) == 1.00:
-                spamCount += 1
-            else:
-                nonspamCount += 1
-        pSpamWord[i] = float(spamCount) / (spamCount + nonspamCount)
-        pNonspamWord[i] = float(nonspamCount) / (spamCount + nonspamCount)
-        # print("spam + nonspam: "spamCount + nonspamCount)
-    return pSpamWord, pNonspamWord
+	print(spamMail, "\t", nonspamMail)
+	
+	global pSpamWord, pNonspamWord
+	pSpamWord = [0 for x in range(len(dataset[0]) - 1)]
+	pNonspamWord = [0 for x in range(len(dataset[0]) - 1)]
+	# total = len(dataset)
+	for i in range(len(dataset[0]) - 1):
+		spamCount, nonspamCount = 0, 0
+		for line in dataset:
+			if(line[-1]) == 1.00:
+				spamCount += 1
+			else:
+				nonspamCount += 1
+		
+		pSpamWord[i] = float(spamCount) / (spamCount + nonspamCount)
+		pNonspamWord[i] = float(nonspamCount) / (spamCount + nonspamCount)
+		# print("spam + nonspam: "spamCount + nonspamCount)
+	return pSpamWord, pNonspamWord
 
 
 def crossValidation(dataset, pSpamWord, pNonspamWord):
-    seed(90)
-    accuracy = 0
-    for _ in range(0, 10):
-        test = []
-        train = []
-        index = []
-        for _ in range(0, ceil(len(dataset) / 10.0)):
-            # for _ in range(0, 2):
-            i = randrange(0, len(dataset))
-            index.append(i)
-            test.append(dataset[i])
+	seed(10)
+	accuracy = 0
+	for _ in range(0, 10):
+		test = []
+		train = []
+		index = []
+		for _ in range(0, ceil(len(dataset) / 10.0)):
+			# for _ in range(0, 2):
+			i = randrange(0, len(dataset))
+			index.append(i)
+			test.append(dataset[i])
 
-        for i in range(0, len(dataset)):
-            if i not in index:
-                train.append(dataset[i])
+		for i in range(0, len(dataset)):
+			if i not in index:
+				train.append(dataset[i])
 
-        recentAccuracy = checkTestset(test, pSpamWord, pNonspamWord)
+		recentAccuracy = checkTestset(test, pSpamWord, pNonspamWord)
+		accuracy += recentAccuracy
+
+	accuracy /= 10
+	print("Accuracy: ", accuracy, "%")
 
 
 def checkTestset(test, pSpamWord, pNonspamWord):
-    '''
-    CHECK
-    '''
+	totalTestdata = len(test)
+	correctlyClassified = 0
+
+	for line in test:
+		global spamMail, nonspamMail
+
+		pSpam, pNonspam = 1, 1
+		pSpam *= spamMail
+		pNonspam *= nonspamMail
+
+		for i in range(len(line) - 1):			
+			pSpam *= pSpamWord[i]
+			pNonspam *= pNonspamWord[i]
+
+		if pSpam > pNonspam and line[-1] == 1.00:
+			correctlyClassified += 1
+		elif pSpam < pNonspam and line[-1] == 0.00:
+			correctlyClassified += 1
+
+	recentAccuracy = (correctlyClassified * 100.0) / totalTestdata
+	print('{0:0.2f} %'.format(recentAccuracy))
+
+	return recentAccuracy
+
 
 
 if __name__ == "__main__":
-    main()
+	main()
